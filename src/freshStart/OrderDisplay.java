491812a9f5b32db1878bee52fe1b22246948835d
mayaj <mayaj@DESKTOP-0I4I6TL>
2022-09-26 12:22:39 -0700

    basic item selection

--- src/freshStart/OrderDisplay.java
+++ src/freshStart/OrderDisplay.java
@@ -1,6 +1,7 @@
 /**
  * OrderDisplay
  * Create a visual display of the information in the orders given.
+ * Allow the user to select orders, items, and individual items. 
  */
 
 package freshStart;
@@ -19,9 +20,11 @@ import javax.swing.JPanel;
 import javax.swing.border.Border;
 
 public class OrderDisplay {
-	private ArrayList<Order> ordersSelected = new ArrayList<Order>();
-	private ArrayList<JCheckBox> companyCheckBoxes = new ArrayList<>();
 	private ArrayList<Order> allOrders = new ArrayList<>();
+	private ArrayList<Order> ordersSelected = new ArrayList<Order>();
+	private ArrayList<Item> itemsSelected = new ArrayList<>();
+	private ArrayList<JCheckBox> companyCheckBoxes = new ArrayList<>();
+	private ArrayList<ArrayList<JCheckBox>> itemCheckBoxes = new ArrayList<>();
 	
 	/**
 	 * Display the information about the given orders to the screen.
@@ -42,6 +45,8 @@ public class OrderDisplay {
 	/**
 	 * Put the information from each order into a separate panel,
 	 * and return the parent panel containing all of the order panels.
+	 * Each order panel contains the orders name and order checkbox,
+	 * and each item in the order with an individual checkbox. 
 	 * @return a panel with one child panel containing the information for each order
 	 */
 	private JPanel getOrderPanel() {
@@ -51,18 +56,23 @@ public class OrderDisplay {
 			Order o = allOrders.get(i);
 			VPanel p = new VPanel();
 			p.setBorder(blackline);
-			addCompanyAndSelection(p, o, i);
-			addStringsToPanel(p, o.toString());
+			addCompanyAndSelection(p, o);
+			addItemsToPanel(p, o);
 			panel.add(p);
 		}
 		panel.setPreferredSize(new Dimension(allOrders.size() * 200,500));
 		return panel;
 	}
 	
-	private void addCompanyAndSelection(JPanel panel, Order order, int index) {
+	/**
+	 * Add the company name and company checkbox to a panel.
+	 * @param panel the panel to add to
+	 * @param order the order whose name to add
+	 */
+	private void addCompanyAndSelection(JPanel panel, Order order) {
 		HPanel compPanel = new HPanel();
 		JCheckBox box = new JCheckBox(); 
-		box.addActionListener(new CompanyCheckListener(box, index));
+		box.addActionListener(new OrderCheckListener(box, order));
 		companyCheckBoxes.add(box);
 		compPanel.add(box);
 		compPanel.add(new JLabel(order.getCompany()));
@@ -70,42 +80,81 @@ public class OrderDisplay {
 	}
 	
 	/**
-	 * Given a string, add each new line in the string
-	 * to the panel as a separate label.
-	 * @param panel the panel to add the lines in the string to
-	 * @param str the string to add to the panel
+	 * For the given order, add each item in the order to the panel
+	 * with its own checkbox.
+	 * @param panel the panel to add to
+	 * @param order the order to add the items for
 	 */
-	private void addStringsToPanel(JPanel panel, String str) {
-		while (str.contains("\n")) {
-			int index = str.indexOf("\n");
-			panel.add(new JLabel(str.substring(0, index)));
-			str = str.substring(index+1);
+	private void addItemsToPanel(JPanel panel, Order order) {
+		ArrayList<Item> items = order.getItems();
+		for (int i = 0; i < items.size(); i++) {
+			Item currItem = items.get(i);
+			HPanel itemPanel = new HPanel();
+			JCheckBox box = new JCheckBox(); 
+			box.addActionListener(new ItemCheckListener(box, currItem));
+			itemPanel.add(box);
+			itemPanel.add(new JLabel(currItem.getQuantity() + " " + currItem.getProductName()));
+			itemPanel.setPreferredSize(new Dimension(150, 50));
+			panel.add(itemPanel);
 		}
-		panel.add(new JLabel(str));
 	}
 	
-	public ArrayList<Order> getOrdersSelected() {
-		return ordersSelected;
-	}
-	
-	private class CompanyCheckListener implements ActionListener {
-		private int index;
+	/**
+	 * OrderCheckListener
+	 * When selected, adds its associated order to the list of orders selected. 
+	 * When deselected, removes its associated order from the list of orders selected. 
+	 */
+	private class OrderCheckListener implements ActionListener {
+		private Order order;
 		private JCheckBox box;
 		
-		public CompanyCheckListener(JCheckBox b, int i) {
-			index = i;
+		public OrderCheckListener(JCheckBox b, Order o) {
+			order = o;
 			box = b;
 		}
 		
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (box.isSelected()) {
-				ordersSelected.add(allOrders.get(index));
+				ordersSelected.add(order);
 			}
 			else {
-				ordersSelected.remove(allOrders.get(index));
+				ordersSelected.remove(order);
 			}
 		}
+	}
+	
+	/**
+	 * ItemCheckListener
+	 * When selected, adds its associated item to the list of items selected. 
+	 * When deselected, removes its associated item from the list of items selected. 
+	 */
+	private class ItemCheckListener implements ActionListener {
+		private Item item;
+		private JCheckBox box;
 		
+		public ItemCheckListener(JCheckBox b, Item i) {
+			item = i;
+			box = b;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (box.isSelected()) {
+				itemsSelected.add(item);
+			}
+			else {
+				itemsSelected.remove(item);
+			}
+		}
+	}
+	
+	// Getters and setters	
+	public ArrayList<Order> getOrdersSelected() {
+		return ordersSelected;
+	}
+	
+	public ArrayList<Item> getItemsSelected() {
+		return itemsSelected;
 	}
 }
