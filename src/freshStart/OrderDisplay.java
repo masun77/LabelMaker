dd87d3d9bfc81d81a451dfd6d45c984e069b737d
mayaj <mayaj@DESKTOP-0I4I6TL>
2022-09-26 12:04:26 -0700

    general order selection

--- src/freshStart/OrderDisplay.java
+++ src/freshStart/OrderDisplay.java
@@ -7,47 +7,66 @@ package freshStart;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
 public class OrderDisplay {
+	private ArrayList<Order> ordersSelected = new ArrayList<Order>();
+	private ArrayList<JCheckBox> companyCheckBoxes = new ArrayList<>();
+	private ArrayList<Order> allOrders = new ArrayList<>();
+	
 	/**
 	 * Display the information about the given orders to the screen.
 	 * @param orders the orders to display
 	 */
-	public void displayOrders(ArrayList<Order> orders) {
+	public JFrame displayOrders(ArrayList<Order> orders) {
 		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // todo change
 		
-		JPanel ordsPanel = getOrderPanel(orders);
+		allOrders = orders;
+		JPanel ordsPanel = getOrderPanel();
 		
 		frame.add(ordsPanel);
 		frame.setSize(ordsPanel.getPreferredSize());
 		frame.setVisible(true);
+		return frame;
 	}
 	
 	/**
 	 * Put the information from each order into a separate panel,
 	 * and return the parent panel containing all of the order panels.
-	 * @param orders the orders to put into the panels
 	 * @return a panel with one child panel containing the information for each order
 	 */
-	private JPanel getOrderPanel(ArrayList<Order> orders) {
+	private JPanel getOrderPanel() {
 		Border blackline = BorderFactory.createLineBorder(Color.black);
 		HPanel panel = new HPanel();
-		for (Order o: orders) {
+		for (int i = 0; i < allOrders.size(); i++) {
+			Order o = allOrders.get(i);
 			VPanel p = new VPanel();
 			p.setBorder(blackline);
+			addCompanyAndSelection(p, o, i);
 			addStringsToPanel(p, o.toString());
 			panel.add(p);
 		}
-		panel.setPreferredSize(new Dimension(orders.size() * 200,500));
+		panel.setPreferredSize(new Dimension(allOrders.size() * 200,500));
 		return panel;
+	}
+	
+	private void addCompanyAndSelection(JPanel panel, Order order, int index) {
+		HPanel compPanel = new HPanel();
+		JCheckBox box = new JCheckBox(); 
+		box.addActionListener(new CompanyCheckListener(box, index));
+		companyCheckBoxes.add(box);
+		compPanel.add(box);
+		compPanel.add(new JLabel(order.getCompany()));
+		panel.add(compPanel);
 	}
 	
 	/**
@@ -63,5 +82,30 @@ public class OrderDisplay {
 			str = str.substring(index+1);
 		}
 		panel.add(new JLabel(str));
+	}
+	
+	public ArrayList<Order> getOrdersSelected() {
+		return ordersSelected;
+	}
+	
+	private class CompanyCheckListener implements ActionListener {
+		private int index;
+		private JCheckBox box;
+		
+		public CompanyCheckListener(JCheckBox b, int i) {
+			index = i;
+			box = b;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (box.isSelected()) {
+				ordersSelected.add(allOrders.get(index));
+			}
+			else {
+				ordersSelected.remove(allOrders.get(index));
+			}
+		}
+		
 	}
 }
--- src/freshStart/OrderDisplayTester.java
+++ src/freshStart/OrderDisplayTester.java
@@ -8,9 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
+import javax.swing.JFrame;
+
 import org.junit.jupiter.api.Test;
 
 class OrderDisplayTester {
+	private static volatile boolean windowOpen = true;
 
 	//@Test
 	void test() {
@@ -22,7 +25,7 @@ class OrderDisplayTester {
 		}
 	}
 	
-	@Test
+	// @Test
 	void testWcontent() {
 		OrderDisplay od = new OrderDisplay();
 		ArrayList<Order> ords = new ArrayList<Order>();
@@ -46,5 +49,34 @@ class OrderDisplayTester {
 		while (true) {
 			
 		}
+	}
+	
+	@Test
+	void testSelection() {
+		OrderDisplay od = new OrderDisplay();
+		ArrayList<Order> ords = new ArrayList<Order>();
+		Order o = new Order("Maya", "1234", "RDF 2", 57832, new DateImp(9,20,2022));
+		o.addItem(new Item("Maya", new DateImp(9,20,22), "Lettuce", 
+				"LG10", "818180000", "10 lbs/cs", 2, 25));
+		o.addItem(new Item("Maya", new DateImp(9,20,22), "Garlic", 
+				"GRG10", "818181110", "10 lbs/cs", 1, 90));
+		o.addItem(new Item("Maya", new DateImp(9,20,22), "Carrots", 
+				"CAR25", "818182220", "25 lbs/cs", 3, 75));
+		ords.add(o);
+		Order o2 = new Order("Joe", "AAA", "RDF 1", 1111, new DateImp(9,20,2022));
+		o2.addItem(new Item("Joe", new DateImp(9,20,22), "Lettuce", 
+				"LG10", "818180000", "10 lbs/cs", 1, 13));
+		o2.addItem(new Item("Maya", new DateImp(9,20,22), "Leeks", 
+				"KRL20", "818183330", "20 lbs/cs", 1, 50));
+		ords.add(o2);
+		
+		JFrame f = od.displayOrders(ords);
+		ArrayList<Order> ordsSelected = od.getOrdersSelected();
+		while (windowOpen) {	// Cool! not sure I'm using volatile right there, but it works!
+			windowOpen = f.isVisible();
+		}
+		
+		assertEquals(1, ordsSelected.size());
+		assertTrue("Maya".equals(ordsSelected.get(0).getCompany()));
 	}
 }
